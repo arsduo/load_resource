976835ea2ed4f17bf0cccbaa3288d1bd94ed72ea
Alex Koppel <alex@alexkoppel.com>
2017-06-30 18:30:56 -0500

    Update scope docs

--- lib/load_resource/scope.ex
+++ lib/load_resource/scope.ex
@@ -2,7 +2,7 @@ defmodule LoadResource.Scope do
   @moduledoc """
   This module defines a scope that can be used in validating a resouce.
 
-  A simple example: we have books and citations. When loading a citation, we want to validate that it belongs to a valid book -- that is, to add `citation.book_id = valid_book_id}` to our SQL query.
+  A simple example: we have books and citations. When loading a citation, we want to validate that it belongs to a valid book -- that is, to add `citation.book_id = ${valid_book_id}` to our SQL query.
 
   Scopes contain two attributes:
 
@@ -14,7 +14,7 @@ defmodule LoadResource.Scope do
   * a primitive (atom, string, number, or boolean), in which case it is used in the SQL query
   * a map or struct containing an `:id` key, in which case the id value is used
 
-  Any other value will result in an UnprocessableValueError being raised.
+  Any other value will result in an `LoadResource.Scope.UnprocessableValueError` being raised.
   """
 
   @enforce_keys [:foreign_key, :accessor]
@@ -22,27 +22,12 @@ defmodule LoadResource.Scope do
 
   alias LoadResource.Scope
 
-  defmodule UnprocessableValueError do
-    @moduledoc """
-    An error raised when Scope.evaluate encounters a result type it doesn't know how to process.
-    """
-    defexception [:message, :value]
-
-    @doc false
-    def exception(value) do
-      %Scope.UnprocessableValueError{
-        message: "Unable to handle result of scope accessor. Expected atom | string | %{id: id} struct.",
-        value: value
-      }
-    end
-  end
-
   @doc """
   A convenience method for creating scopes for earlier loaded resources.
 
   `Scope.from_atom(:book)` is equivalent to writing:
 
-  ```elixir
+  ```
   %Scope{
     foreign_key: :book_id,
     accessor: fn(conn) -> conn.assigns[:book]
@@ -51,7 +36,7 @@ defmodule LoadResource.Scope do
 
   In pseudo-SQL, that scope turns into `where book_id = {conn.assigns[:book].id}`.
 
-  (This assumes `conn.assigns[:book]` is an appropriate value, as it will be if it's a something previously loaded by LoadResource.Plug.)
+  (This assumes `conn.assigns[:book]` is an appropriate value, as it will be if it's a something previously loaded by `LoadResource.Plug`.)
   """
   def from_atom(scope_key) when is_atom(scope_key) do
     %Scope{
@@ -61,9 +46,18 @@ defmodule LoadResource.Scope do
   end
 
   @doc """
-  Run a scope on a given `conn` object and return the value for use in the QueryBuilder.
+  Run a scope on a given `conn` object and return the value for use by `LoadResource.QueryBuilder`.
 
   If needed, this method will transform the result of the `accessor` function into an appropriate value (for instance, from a map containing an `:id` key to the appropriate value).
+
+  Given this scope and an `identify_source_book_id/1` function that returns either `"foo"` or `%{id: "foo"}`:
+
+  ```
+  scope = %Scope{foreign_key: :source_book_id, accessor: &identify_source_book_id/1}
+  ```
+
+  `Scope.evaluate(scope)` will return "foo".
+  ```
   """
   def evaluate(%Scope{accessor: accessor}, conn) do
     process_scope_value(accessor.(conn))
@@ -75,6 +69,6 @@ defmodule LoadResource.Scope do
   defp process_scope_value(value) when is_boolean(value), do: value
   defp process_scope_value(%{id: id}), do: id
   defp process_scope_value(value) do
-    raise UnprocessableValueError, value
+    raise Scope.UnprocessableValueError, value
   end
 end
