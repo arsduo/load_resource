f9f0e763fb1687bd2b74afb04c7eafe63a9250f7
Alex Koppel <alex@alexkoppel.com>
2017-06-30 18:31:16 -0500

    Extract UnprocessableValueError with docs

---
+++ lib/load_resource/unprocessable_value_error.ex
@@ -0,0 +1,17 @@
+defmodule LoadResource.Scope.UnprocessableValueError do
+  @moduledoc """
+  An error raised when `LoadResource.Scope.evaluate/2` encounters a result type it doesn't know how to process.
+  """
+
+  alias LoadResource.Scope.UnprocessableValueError
+
+  defexception [:message, :value]
+
+  @doc false
+  def exception(value) do
+    %UnprocessableValueError{
+      message: "Unable to handle result of scope accessor. Expected atom | string | %{id: id} struct.",
+      value: value
+    }
+  end
+end
